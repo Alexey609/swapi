1de5613a65cb51ac39be118648b619488c216ca6
Alexey609 <alexxx609@mail.ru>
2023-06-03 16:38:34 +0500

    fix: item data check

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -14,20 +14,20 @@ export const Item = () => {
          }
     }, [id]);
 
-    return (
+    return item?.data ? (
         <div className={styles.detail}>
             <div>
-                <h4>{item?.data?.name}</h4>
+                <h4>{item.data.name}</h4>
             </div>
             <div className={styles.body}>
-                <p>Климат - {item?.data?.climate}</p>
-                <p>Диаметр - {item?.data?.diameter}</p>
-                <p>Гравитация - {item?.data?.gravity}</p>
-                <p>Население - {item?.data?.population}</p>
-                <p>Местность - {item?.data?.terrain}</p>
-                <p>Период - {item?.data?.orbital_period}</p>
+                <p>Климат - {item.data.climate}</p>
+                <p>Диаметр - {item.data.diameter}</p>
+                <p>Гравитация - {item.data.gravity}</p>
+                <p>Население - {item.data.population}</p>
+                <p>Местность - {item.data.terrain}</p>
+                <p>Период - {item.data.orbital_period}</p>
             </div>
             <Link to="/planets" className={styles.link}>Вернуться</Link>
         </div>
-    );
+    ) : null;
 };
